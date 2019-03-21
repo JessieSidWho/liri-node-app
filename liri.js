6aed485cb4049452644b782ff0ea8e263dae9fa0
Jessie Sidhu <jsidhu22@gmail.com>
2019-03-21 10:17:26 -0700

    functions and function defaults working

--- liri.js
+++ liri.js
@@ -11,9 +11,14 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
 // OMDB
-if (process.argv[2] === "movie-this") {
-
-    var movieName = process.argv.slice(3).join(" ");
+let runMovie = function(){
+    let movieName = ""
+    if ( process.argv[3] != undefined){
+        movieName = process.argv.slice(3).join("+");
+    }
+    else{
+        return console.log(`If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/> It's on Netflix!`);
+    }
 
     axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy").then(
         function(response) {
@@ -32,9 +37,16 @@ if (process.argv[2] === "movie-this") {
 
 
 // Bands In Town
-if (process.argv[2] === "concert-this") {
+let runBands = function(){
 
-    let artist = process.argv.slice(3).join("+"); 
+    let artist = ""
+    if ( process.argv[3] != undefined){
+        artist = process.argv.slice(3).join("+");
+    }
+    else{
+        return console.log("Seems like you didn't request any information.");
+    }
+
     let queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
     axios.get(queryURL).then(function(response) {
@@ -63,83 +75,121 @@ if (process.argv[2] === "concert-this") {
 }
 
 
-// Spotify 
-
-if (process.argv[2] === "spotify-this-song") {
-
-    let songName = process.argv.slice(3).join("+");
-    // console.log(songName);
-
-    if(process.argv[2] === "spotify-this-song"){
-        spotify.search({ type: 'track', query: songName, limit: 1 }, function(err, data) {
-            if (err) {
+// Spotify
+let runSpotify = function(){
+    let songName = ""
+    if ( process.argv[3] != undefined){
+        songName = process.argv.slice(3).join("+");
+    }
+    else{
+        return defaultSpotify(); 
+    }
+    
+    spotify.search({ type: 'track', query: songName, limit: 1 }, function(err, data) {
+        if (err) {
             fs.appendFileSync('log.txt', `Error occurred: ${err}`, 'utf8');
             return console.log(`Error occurred: ${err}`);
-            }
-        
-            for (let i = 0; i < data.tracks.items.length; i++) {
-                console.log(`
-                Song Title: ${data.tracks.items[i].name}`);
-                fs.appendFileSync('log.txt', `
-                Song Title: ${data.tracks.items[i].name}`, 'utf8'); 
-                
-                console.log(`
-                Album Title: ${data.tracks.items[i].album.name}`);
-                fs.appendFileSync('log.txt', `
-                Album Title: ${data.tracks.items[i].album.name}`, 'utf8');
-                
-                console.log(`
-                Artist(s) Name: ${data.tracks.items[i].artists[0].name}`);
-                fs.appendFileSync('log.txt', `
-                Artist(s) Name: ${data.tracks.items[i].artists[0].name}`, 'utf8');
-                
-                console.log(`
-                Preview URL: ${data.tracks.items[i].preview_url}`);
-                fs.appendFileSync('log.txt', `
-                Preview URL: ${data.tracks.items[i].preview_url}
-                /n`, 'utf8');
-            }
-            });
-    } else { //The Sign by Ace of Base
-        spotify.search({ type: 'track', query: 'The Sign', limit: 1 }, function(err, data) {
-            if (err) {
-                fs.appendFileSync('log.txt', `
-                Error occurred: ${err}`, 'utf8');
-                return console.log(`
-                Error occurred: ${err}`);
-            }
-        
-            console.log(`You did not enter a song title. Here is a recommendation:`);
-        
-            for (let i = 0; i < data.tracks.items.length; i++) {
-                if (data.tracks.items[i].artists[0].name === "Ace of Base") {
-                    console.log(`
-                    Song Title: ${data.tracks.items[i].name}`);
-                    fs.appendFileSync('log.txt', `
-                    Song Title: ${data.tracks.items[i].name}`, 'utf8'); 
-                    
-                    console.log(`
-                    Album Title: ${data.tracks.items[i].album.name}`);
-                    fs.appendFileSync('log.txt', `
-                    Album Title: ${data.tracks.items[i].album.name}`, 'utf8');
-                    
-                    console.log(`
-                    Artist(s) Name: ${data.tracks.items[i].artists[0].name}`);
-                    fs.appendFileSync('log.txt', `
-                    Artist(s) Name: ${data.tracks.items[i].artists[0].name}`, 'utf8');
-                    
-                    console.log(`
-                    Preview URL: ${data.tracks.items[i].preview_url}`);
-                    fs.appendFileSync('log.txt', `
-                    Preview URL: ${data.tracks.items[i].preview_url}
-                    `, 'utf8');
-                }
-            }
-        });
-    }
+        }
+    
+        for (let i = 0; i < data.tracks.items.length; i++) {
+            console.log(`
+            Song Title: ${data.tracks.items[i].name}`);
+            fs.appendFileSync('log.txt', `
+            Song Title: ${data.tracks.items[i].name}`, 'utf8'); 
+            
+            console.log(`
+            Album Title: ${data.tracks.items[i].album.name}`);
+            fs.appendFileSync('log.txt', `
+            Album Title: ${data.tracks.items[i].album.name}`, 'utf8');
+            
+            console.log(`
+            Artist(s) Name: ${data.tracks.items[i].artists[0].name}`);
+            fs.appendFileSync('log.txt', `
+            Artist(s) Name: ${data.tracks.items[i].artists[0].name}`, 'utf8');
+            
+            console.log(`
+            Preview URL: ${data.tracks.items[i].preview_url}`);
+            fs.appendFileSync('log.txt', `
+            Preview URL: ${data.tracks.items[i].preview_url}
+            /n`, 'utf8');
+        }
 
+
+    });
 }
 
 
-// Do What It Says
+// Default Spotify Song
+let defaultSpotify = function(){
+  spotify.search({ type: 'track', query: 'The Sign', limit: 10 }, function(err, data) 
+  {
+    if (err) {
+        fs.appendFileSync('log.txt', `
+        Error occurred: ${err}`, 'utf8');
+        return console.log(`
+        Error occurred: ${err}`);
+    }
 
+    console.log(`You did not enter a song title. Here is a recommendation:`);
+    // console.log(data.tracks.items.length)
+    for (let i = 0; i < data.tracks.items.length; i++) {
+        
+        if (data.tracks.items[i].artists[0].name === "Ace of Base") {
+            console.log(`
+            Song Title: ${data.tracks.items[i].name}`);
+            fs.appendFileSync('log.txt', `
+            Song Title: ${data.tracks.items[i].name}`, 'utf8'); 
+            
+            console.log(`
+            Album Title: ${data.tracks.items[i].album.name}`);
+            fs.appendFileSync('log.txt', `
+            Album Title: ${data.tracks.items[i].album.name}`, 'utf8');
+            
+            console.log(`
+            Artist(s) Name: ${data.tracks.items[i].artists[0].name}`);
+            fs.appendFileSync('log.txt', `
+            Artist(s) Name: ${data.tracks.items[i].artists[0].name}`, 'utf8');
+            
+            console.log(`
+            Preview URL: ${data.tracks.items[i].preview_url}`);
+            fs.appendFileSync('log.txt', `
+            Preview URL: ${data.tracks.items[i].preview_url}
+            `, 'utf8');
+        }
+        
+    }
+  });
+}
+
+
+
+// IF / ELSE
+if (process.argv[2] === "concert-this") {
+    runBands();
+} else if (process.argv[2] === "movie-this") {
+    runMovie();
+} else if(process.argv[2] === "spotify-this-song"){
+    runSpotify();
+} else if (process.argv[2] === "do-what-it-says") {
+
+    fs.readFile("random.txt", "utf8", function(error, data) {
+  
+        if(error){
+          fs.appendFileSync('log.txt', error, 'utf8');
+          return console.log(error);
+        }
+
+        var dataArr = data.split(",");
+
+        if (dataArr[0] === "concert-this") {
+            runBands();
+        }
+        if (dataArr[0] === "movie-this") {
+            runMovie();
+        }
+        if (dataArr[0] === "spotify-this-song") {
+            runSpotify();
+        }
+    
+    });
+} 
